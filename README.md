aa9b5ea23c4fb6d53c3b1e3405295298b8163ee0
Mark Durrant <mark@pebblecode.com>
2012-04-26 17:26:15 +0100

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ fear-the-IOC
 ============
 
 Fear the IOC (International Olympic Committee) is a bookmarklet that censors any text or images that the IOC may find litigious.
+
---
+++ fear-the-IOC.js
@@ -0,0 +1,118 @@
+/*
+ * jQuery Highlight plugin
+ *
+ * Based on highlight v3 by Johann Burkard
+ * http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html
+ *
+ * Code a little bit refactored and cleaned (in my humble opinion).
+ * Most important changes:
+ *  - has an option to highlight only entire words (wordsOnly - false by default),
+ *  - has an option to be case sensitive (caseSensitive - false by default)
+ *  - highlight element tag and class names can be specified in options
+ *
+ * Usage:
+ *   // wrap every occurrance of text 'lorem' in content
+ *   // with <span class='highlight'> (default options)
+ *   $('#content').highlight('lorem');
+ *
+ *   // search for and highlight more terms at once
+ *   // so you can save some time on traversing DOM
+ *   $('#content').highlight(['lorem', 'ipsum']);
+ *   $('#content').highlight('lorem ipsum');
+ *
+ *   // search only for entire word 'lorem'
+ *   $('#content').highlight('lorem', { wordsOnly: true });
+ *
+ *   // don't ignore case during search of term 'lorem'
+ *   $('#content').highlight('lorem', { caseSensitive: true });
+ *
+ *   // wrap every occurrance of term 'ipsum' in content
+ *   // with <em class='important'>
+ *   $('#content').highlight('ipsum', { element: 'em', className: 'important' });
+ *
+ *   // remove default highlight
+ *   $('#content').unhighlight();
+ *
+ *   // remove custom highlight
+ *   $('#content').unhighlight({ element: 'em', className: 'important' });
+ *
+ *
+ * Copyright (c) 2009 Bartek Szopka
+ *
+ * Licensed under MIT license.
+ *
+ */
+
+jQuery.extend({
+    highlight: function (node, re, nodeName, className) {
+        if (node.nodeType === 3) {
+            var match = node.data.match(re);
+            if (match) {
+                var highlight = document.createElement(nodeName || 'span');
+                highlight.className = className || 'highlight';
+                var wordNode = node.splitText(match.index);
+                wordNode.splitText(match[0].length);
+                var wordClone = wordNode.cloneNode(true);
+                highlight.appendChild(wordClone);
+                wordNode.parentNode.replaceChild(highlight, wordNode);
+                return 1; //skip added node in parent
+            }
+        } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
+                !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+                !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+            for (var i = 0; i < node.childNodes.length; i++) {
+                i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+            }
+        }
+        return 0;
+    }
+});
+
+jQuery.fn.unhighlight = function (options) {
+    var settings = { className: 'highlight', element: 'span' };
+    jQuery.extend(settings, options);
+
+    return this.find(settings.element + "." + settings.className).each(function () {
+        var parent = this.parentNode;
+        parent.replaceChild(this.firstChild, this);
+        parent.normalize();
+    }).end();
+};
+
+jQuery.fn.highlight = function (words, options) {
+    var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
+    jQuery.extend(settings, options);
+    
+    if (words.constructor === String) {
+        words = [words];
+    }
+    words = jQuery.grep(words, function(word, i){
+      return word != '';
+    });
+    words = jQuery.map(words, function(word, i) {
+      return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    });
+    if (words.length == 0) { return this; };
+
+    var flag = settings.caseSensitive ? "" : "i";
+    var pattern = "(" + words.join("|") + ")";
+    if (settings.wordsOnly) {
+        pattern = "\\b" + pattern + "\\b";
+    }
+    var re = new RegExp(pattern, flag);
+    
+    return this.each(function () {
+        jQuery.highlight(this, re, settings.element, settings.className);
+    });
+};
+
+//Fear the IOC by Mark Durrant & pebble {code}
+
+$(document).ready(function () {
+  $("body").highlight(["Olympic","Olympics","Olympian", "Olympiad","Paralympic","Paralympics","Paralympian", "Paralympiad","ioc","ipc","Olympics","Paralympics","games","london","2012","medal","gold","silver","bronze","team GB","get set","games maker","javelin"]);
+  $(".highlight").css({"background":"black","color":"transparent","transform":"rotate(2deg)","-ms-transform":"rotate(2deg)","-moz-transform":"rotate(2deg)","-webkit-transform":"rotate(2deg)","-o-transform":"rotate(2deg)","display": "inline-block"
+  });
+  
+  $('img[src*="Olympic"], img[src*="Olympics"], img[src*="Olympian"], img[src*="Olympiad"], img[src*="Paralympic"], img[src*="Paralympics"], img[src*="Paralympian"], img[src*="Paralympiad"], img[src*="ioc"], img[src*="ipc"], img[src*="games"], img[src*="london"], img[src*="2012"], img[src*="medal"], img[src*="gold"], img[src*="silver"], img[src*="bronze"], img[src*="team GB"], img[src*="get set"], img[src*="games maker"], img[src*="javelin"]').attr("src","http://f.cl.ly/items/1o2d0B2l0r04023n202a/blank.gif").css({"background":"black","transform":"rotate(-1deg)","-ms-transform":"rotate(-1deg)","-moz-transform":"rotate(-1deg)","-webkit-transform":"rotate(-1deg)","-o-transform":"rotate(-1deg)","display": "inline-block"
+  });;
+});
